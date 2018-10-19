3612373d42c1abb88b401362ebdc31da227ee855
Jesse Battalino <acityofwalls@gmail.com>
2018-10-19 22:28:02 +0000

    Clean up navbar and implement routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # Routes to other static pages (this removes the need for pages/resume and is now /resume as a route)
   get "/resume" => "pages#resume"
   get "/code" => "pages#code"
-  get "music" => "pages#music"
+  get "/music" => "pages#music"
   get "/connect" => "pages#connect"
 end
