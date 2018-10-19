2766d188e9b19fb8a3d34c22c823ca5dba3cd84d
Jesse Battalino <acityofwalls@gmail.com>
2018-10-19 06:02:59 +0000

    Create static pages and routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+    def index
+    end
+    
+    def resume
+    end
+    
+    def code
+    end
+    
+    def music
+    end
+    
+    def connect
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Homepage
+  root to: "pages#index"
+  
+  # Routes to other static pages (this removes the need for pages/resume and is now /resume as a route)
+  get "/resume" => "pages#resume"
+  get "/code" => "pages#code"
+  get "music" => "pages#music"
+  get "/connect" => "pages#connect"
+end
