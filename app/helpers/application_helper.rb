608a4a732a1a63ff6b511a6323c2210cde9b202d
Jesse Battalino <acityofwalls@gmail.com>
2018-10-22 18:26:34 +0000

    Implement helper methods for navbar highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    
+    def active_class(link_path)
+        current_page?(link_path) ? "nav-item active" : "nav-item"
+    end
+    
+    def dropdown_active_class()
+        current_page?(python_path) || current_page?(java_path) || current_page?(go_path) || current_page?(swift_path) ? "nav-item dropdown active" : "nav-item dropdown"
+    end
+    
 end
