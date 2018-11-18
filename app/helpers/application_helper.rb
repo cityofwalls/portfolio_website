76a27e07732e33bfccdc67df96285aaf290e24a7
Jesse Battalino <acityofwalls@gmail.com>
2018-11-18 19:20:14 +0000

    Update navbar highlighting to include ML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     end
     
     def dropdown_active_class()
-        current_page?(python_path) || current_page?(java_path) || current_page?(go_path) || current_page?(swift_path) ? "nav-item dropdown active" : "nav-item dropdown"
+        current_page?(python_path) || current_page?(java_path) || current_page?(go_path) || current_page?(swift_path) || current_page?(ml_path) ? "nav-item dropdown active" : "nav-item dropdown"
     end
     
 end
